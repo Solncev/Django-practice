fffb7d3e8371c25135ff8e02fe417b9dabf3640a
khaliullin <sgtkhaliullin@gmail.com>
2016-11-14 23:44:22 +0300

    Python Homework tast3 made.

--- push.py
+++ push.py
@@ -2,5 +2,5 @@ from subprocess import call
 message = input("Commit message: ")
 call(["git", "add", "."])
 call(["git", "commit", "-m %s" %(message)])
-# call(["git", "pull", "origin", "master"])
-# call(["git", "push", "origin", "master"])
+call(["git", "pull", "origin", "Sagit"])
+call(["git", "push", "origin", "Sagit"])
